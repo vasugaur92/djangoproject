384cfc0edded083c1d7207110d2554bad0d2b38d
Vasu <vasu04111@gmail.com>
2017-01-10 17:22:10 +0530

    fixed the error in the reverse function

--- music/urls.py
+++ music/urls.py
@@ -6,9 +6,17 @@ app_name = 'music'
 urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(), name='index'),
+
     # /music/21
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    #music/album/add
-    url(r'^album/add/$', views.AlbumCreate.as_view(), name='Album-add'),
+
+    # music/album/add
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # music/album/2/ <- for update album
+    url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # music/album/2/delete/  <- to delete album
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 
 ]
